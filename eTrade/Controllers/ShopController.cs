3cbd6efa1259363f7d2428c6bc27d7ebc65cddcb
Sabya Roy <sabya.citik@gmail.com>
2023-06-15 13:58:09 +0600

    Add project files.

---
+++ eTrade/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eTrade.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
